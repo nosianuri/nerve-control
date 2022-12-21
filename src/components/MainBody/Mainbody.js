20f393e84390c774aec238886e9a83e867368071
nosianuri <nurinosia@gmail.com>
2022-12-22 03:27:01 +0600

    add tracking link

--- src/components/MainBody/Mainbody.js
+++ src/components/MainBody/Mainbody.js
@@ -26,33 +26,33 @@ const Mainbody = () => {
           <p className='lg:text-5xl text-3xl font-bold text-red-600'>SALE</p>
           {/* <p className='text-xl'>Up To 50% Off</p> */}
         </div>
-        <a href="https://www.r3hlvtrk.com/3J67C/7ZRJQL1">
+        <a href="https://www.r3hlvtrk.com/3J67C/7ZRJQL1/">
         <img className='lg:w-56 w-28   mx-auto lg:mx-0 rounded-lg lg:mt-10 ' src={cristmas} alt="" />
         </a>
         
         </div>
         
         <div>
-        <a href="https://www.r3hlvtrk.com/3J67C/7ZRJQL1">
+        <a href="https://www.r3hlvtrk.com/3J67C/7ZRJQL1/">
         <img className='mx-auto  lg:w-2/5 w-64  lg:my-16 my-10' src={mainimg} alt="" />
         </a>
         
         </div>
         
-        <button className='px-3 py-2 lg:text-lg mx-10 lg:mx-0 rounded-lg mb-10 bg-[#0f8548] leading-normal  hover:bg-[#0f8548]'><a className='mb-2' href="https://www.r3hlvtrk.com/3J67C/7ZRJQL1">Click Here and Access The Official Website Now</a></button>
-        <a href="https://www.r3hlvtrk.com/3J67C/7ZRJQL1">
+        <button className='px-3 py-2 lg:text-lg mx-10 lg:mx-0 rounded-lg mb-10 bg-[#0f8548] leading-normal  hover:bg-[#0f8548]'><a className='mb-2' href="https://www.r3hlvtrk.com/3J67C/7ZRJQL1/">Click Here and Access The Official Website Now</a></button>
+        <a href="https://www.r3hlvtrk.com/3J67C/7ZRJQL1/">
         <img className='mx-auto mb-3 lg:w-96 w-1/2' src={gauranted} alt="" />
         </a>
         
-        <a href="https://www.r3hlvtrk.com/3J67C/7ZRJQL1">
+        <a href="https://www.r3hlvtrk.com/3J67C/7ZRJQL1/">
         <img className='mx-auto mb-32 lg:w-96 w-1/2' src={imglogo} alt="" />
         </a>
         
         
 
         <div className='lg:text-lg text-sm lg:pt-32 pt-10'>
-            <a href="">Terms of Use </a>|
-            <a href=""> Privacy Policy</a>
+            <a href="https://www.r3hlvtrk.com/3J67C/7ZRJQL1/">Terms of Use </a>|
+            <a href="https://www.r3hlvtrk.com/3J67C/7ZRJQL1/"> Privacy Policy</a>
         </div>
     </div>
     </div>
